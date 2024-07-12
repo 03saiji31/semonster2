0b8e70283d0735be67e34b96b4e922c81418a345
haru-15 <m1m24a33@oit.ac.jp>
2024-07-12 18:31:03 +0900

    nkm monsterの追加

---
+++ app/src/main/java/org/example/Monster.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Monster {
+  private String name;
+  private int rare;
+
+  Monster(int nnum, int rnum) {
+    this.name = this.summonMonster(nnum);
+    this.rare = rnum;
+  }
+
+  String summonMonster(int mnunber) {
+    String monsters[] = {"スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント"};
+
+    return monsters[mnunber];
+  }
+
+  @Override
+  public String toString() {
+
+    return this.name + ":レア度[" + this.rare + "]\n";
+  }
+}
---
+++ app/src/test/java/org/example/MonsterTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MonsterTest {
+
+  @Test
+  public void testMonsterToString() {
+    Monster classUnderTest = new Monster();
+    assertNotNull("スライム:レア度[0]\n", classUnderTest.toString());
+  }
+}
