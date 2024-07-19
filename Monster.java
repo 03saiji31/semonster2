949fcb7941d0a2dd7c2d140271f9d6b37e975be1
shinnosuke <m1m24a29@oit.ac.jp>
2024-07-19 14:11:33 +0900

    add-Player-bytouda

--- Monster.java
+++ Monster.java
@@ -1,14 +1,22 @@
 import java.util.Random;
 
 public class Monster {
-  String name;
-  int rare;
+  private String name;
+  private int rare; // 戦うとレア度が高いほうが勝つ．同じ場合は引き分け
 
-  Monster() {
-    Random random = new Random();
-    int a = random.nextInt(5);
-    this.name = this.summonMonster(a);
-    this.rare = a;
+  Monster(int nameNum, int rareNum) {
+    this.name = this.summonMonster(nameNum);
+    this.rare = rareNum;
   }
 
+  String summonMonster(int mnumber) {
+    String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
+    return monsters[mnumber];
+  }
+
+  @Override
+  public String toString() {
+
+    return this.name + ":レア度[" + this.rare + "]\n";
+  }
 }
